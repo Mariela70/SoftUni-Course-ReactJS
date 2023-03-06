de5c164130b1c20f3778fede9c5a517ac0235fc3
Mariela <marielamiteva97@abv.bg>
2023-03-06 21:19:49 +0200

    client src

---
+++ Workshop Components/client/src/App.js
@@ -0,0 +1,72 @@
+import * as userService from './services/UserService';
+import { useEffect, useState } from 'react';
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import './App.css';
+import { UserList } from "./components/UserList";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    userService.getAll()
+      .then(users => {
+        setUsers(users);
+      })
+      .catch(err => {
+        console.log('Error' + err);
+      });
+  }, []);
+
+  const onUserCreateSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const data = Object.fromEntries(formData);
+
+    const createdUser = await userService.create(data);
+
+    setUsers(state => [...state, createdUser])
+  };
+  const onUserUpdateSubmit = async (e, userId) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const data = Object.fromEntries(formData);
+
+    const updatedUser = await userService.update(userId, data);
+    setUsers(state => state.map(x => x._id === userId ? updatedUser : x));
+  }
+
+  const onUserDelete = async (userId) => {
+    await userService.remove(userId);
+
+    setUsers(state => state.filter(x => x._id !== userId));
+  };
+
+
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <section className="card users-container">
+          < Search />
+
+          <UserList 
+          users={users} 
+          onUserCreateSubmit={onUserCreateSubmit} 
+          onUserUpdateSubmit={onUserUpdateSubmit}
+          onUserDelete={onUserDelete}
+          />
+
+
+        </section>
+      </main>
+      <Footer />
+
+    </>
+  );
+}
+
+export default App;
